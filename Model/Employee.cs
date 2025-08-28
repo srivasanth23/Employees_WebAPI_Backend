6a51c0acc0dc1f79e7e5b7d9e624fba40ed45c84
Tejaswini G H <tejugowda363@gmail.com>
2025-08-28 11:01:38 +0530

    Add project files.

---
+++ Model/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiEmployee.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public required string Role { get; set; }
+
+        [EmailAddress, Required]
+        public string Email { get; set; } 
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
---
+++ Model/EmployeeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiEmployee.Model
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options) : base(options) { }
+
+        protected EmployeeContext() { }
+
+        public DbSet<Employee>  employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+     }
+}
